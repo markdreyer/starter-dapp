03ba5530dcadc293e527c924b91ee015372f8e98
Mark Dreyer <dreyguy@gmail.com>
2021-04-09 17:00:54 -0500

    update testnet DM contract

--- src/config.testnet.ts
+++ src/config.testnet.ts
@@ -36,7 +36,7 @@ export const network: NetworkType = {
   apiAddress: 'https://testnet-api.elrond.com',
   gatewayAddress: 'https://testnet-gateway.elrond.com',
   explorerAddress: 'https://testnet-explorer.elrond.com/',
-  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqf8llllswuedva',
+  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqp8lllls9jsunl',
 };
 
 const networkSchema = object({
